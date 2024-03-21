f8bad1668c94d1d76158367b27a363d1cb005944
Alistair Michael <alistair.michael@uq.edu.au>
2024-03-21 18:29:14 +1000

    chunking

--- libASL/scala_backend.ml
+++ libASL/scala_backend.ml
@@ -5,10 +5,14 @@ open Asl_utils
 
 module StringTbl = Hashtbl.Make(String) ;;
 module IntTbl = Map.Make(Int) ;;
+
+
 module DefSet = Set.Make(struct 
-  type t = (ident * ty) 
-  let compare (a,b) (c,d) = (match (Stdlib.compare a c) with
-      | 0 -> Stdlib.compare b d 
+  type t = (ident * ty * bool) 
+  let compare (a,b,e) (c,d,f) = (match (Stdlib.compare a c) with
+      | 0 -> (match (Stdlib.compare b d ) with 
+        | 0 -> (Stdlib.compare e f )
+        | s -> s)
       | s -> s)
 end)
 
@@ -72,6 +76,7 @@ let global_opens = ["ir"]
 let uniq_counter : int ref = ref 0 
 
 let new_index _ : int = uniq_counter := !uniq_counter + 1 ; !uniq_counter
+let new_indexs (b: string) : string = b ^ (string_of_int (new_index ()))
 
 let get_decl_scope st = 
   ((IntTbl.find_opt  st.indent st.decl_scopes) |> (function 
@@ -97,7 +102,7 @@ let mergeSig (a: fsig) (b: fsig) : fsig =
   
 
 (* Shallow inspection of an expression to guess its type. *)
-let infer_type e =
+let infer_type e : ty option =
     let tint = Some (Type_Constructor (Ident "integer")) in
     let tbool  = Some (Type_Constructor (Ident "boolean"))  in
     let tbits = fun b -> Some (Type_Bits b) in
@@ -129,13 +134,18 @@ let prints_arg_type (t) : string =
   | (Type_Constructor (Ident "integer")) -> "BigInt"
   | (Type_Constructor (Ident "boolean")) -> "Boolean"
   | Type_Constructor (Ident "rt_label") -> "Expr"
+  | Type_Constructor (Ident "any") -> "Any"
+  | Type_Constructor (Ident "unit") -> "Unit"
   | Type_Constructor (Ident "rt_sym") -> "Expr"
   | t -> failwith @@ "Unknown arg type: " ^ (pp_type t)
 
 let prints_ret_type t =
   match t with
-  | Some (Type_Constructor (Ident "boolean")) -> "Boolean"
-  | None -> "Unit"
+  | Some (Type_Constructor (Ident "boolean")) -> ": Boolean"
+  | Some Type_Constructor (Ident "any") -> "Any"
+  | Some Type_Constructor (Ident "unit") -> "Unit"
+  | Some (Type_Tuple l) -> ": (" ^ (String.concat "," (List.map prints_arg_type l) ) ^ ")"
+  | None -> ""
   | Some t -> failwith @@ "Unknown return type: " ^ (pp_type t)
 
 (****************************************************************
@@ -247,6 +257,8 @@ and default_value t st =
   | Type_Constructor (Ident "boolean") -> "true"
   | Type_Constructor (Ident "integer") -> "BigInt(0)"
   | Type_Constructor (Ident "rt_label") -> "null"
+  | Type_Constructor (Ident "unit") -> "null"
+  | Type_Constructor (Ident "any") -> "null"
   | Type_Array(Index_Range(lo, hi),ty) ->
       let lo = prints_expr lo st in
       let hi = prints_expr hi st in
@@ -276,11 +288,11 @@ let write_nl st =
  ****************************************************************)
 
 let write_fun_return e st =
-  let s = Printf.sprintf "(%s)" e in
+  let s = Printf.sprintf "%s" e in
   write_line s st
 
 let write_proc_return st =
-  write_line "()" st
+  write_line "/*proc return */ ()" st
 
 let write_assert s st =
   let s = Printf.sprintf "assert (%s)" s in
@@ -328,12 +340,31 @@ let write_if_end st =
  * Stmt Printing
  ****************************************************************)
 
+
+let prints_lexpr v = 
+  match v with
+  | LExpr_Wildcard -> "_"
+  | LExpr_Var v -> name_of_ident v
+  | LExpr_Array (LExpr_Var v, i) -> name_of_ident v
+  | LExpr_Field (l, f) -> name_of_lexpr l 
+  | LExpr_Tuple (ls) -> "(" ^ String.concat "," (List.map name_of_lexpr ls) ^ ")" 
+  | _ -> failwith @@ "pritns_lexpr: " ^ (pp_lexpr v)
+
+let rec expr_of_lexpr v = 
+  match v with
+  | LExpr_Var v -> Expr_Var v
+  | LExpr_Array (LExpr_Var v, i) -> Expr_Array (Expr_Var v, i)
+  | LExpr_Field (l, f) -> Expr_Field (expr_of_lexpr l , f) 
+  | LExpr_Tuple (ls) -> Expr_Tuple (List.map expr_of_lexpr ls) 
+  | _ -> failwith @@ "expr_of_lexpr: " ^ (pp_lexpr v)
+
 let rec write_assign v e st =
   match v with
   | LExpr_Wildcard ->
-      let s = Printf.sprintf "val _ = %s \n" e in
+      failwith @@ "write_assign: " ^ (pp_lexpr v);
+      (*let s = Printf.sprintf "val _ = %s \n" e in
       st.skip_seq <- true;
-      write_line s st
+      write_line s st*)
 
   | LExpr_Var v ->
       let v = name_of_ident v in
@@ -367,6 +398,12 @@ let rec write_assign v e st =
 
 (*let add_def d t defs =  DefSet.add (d, t) defs *)
 
+
+module ExprSet = Set.Make(struct 
+  type t = lexpr 
+  let compare a b = Stdlib.compare (a) (b)
+end) 
+
 type block = { 
   mutable stmts: stmt list;
   mutable defs: DefSet.t;
@@ -374,7 +411,11 @@ type block = {
 
 type proc = {
   name: ident;
+  rt_return: ty option option;
+  mutable stmt_count : int;
+  mutable returning: bool;
   mutable params: DefSet.t;
+  mutable updated : ExprSet.t;
   mutable blocks: block list;
 }
 
@@ -385,59 +426,114 @@ type funwork = {
   mutable pr: proc;
 }
 
-let init_fn name params: funwork = {procs = []; bl = {stmts = []; defs = params} ; pr = {name=name; params=params; blocks = []}}
+let get_proc_return_value (p: proc) = 
+  let return_temp = ((Ident (new_indexs "ret_"))) in
+  match p.rt_return with 
+    | Some Some s -> [(return_temp, s)]
+    | Some None -> [(return_temp, Type_Constructor (Ident "unit"))]
+    | None -> [] 
 
-let empty_block _ = {stmts=[]; defs = DefSet.empty}
-let continue_block b = {b with stmts=[]}
+let get_proc_return_sig (p: proc) = 
+(* Intermediate functions dont have a return stmt, so they just return their bindings *) 
+  ((get_proc_return_value p) @ (DefSet.to_list p.params |> List.concat_map (function
+    | (a,b,true) -> [(a,b)]
+    | _ -> []))) 
 
-let new_name _ = Ident ( "proc_" ^ (string_of_int (new_index ()))) 
+let get_proc_return_type (p: proc) : ty option = 
+  match (get_proc_return_sig p |> List.map snd) with 
+    | [] -> None
+    | l -> Some (Type_Tuple l)  
+  
 
-let add_stmt f (s: stmt) = !f.bl.stmts <- (!f.bl.stmts @ [s]) 
 
-let add_def f d t = !f.bl.defs <- (DefSet.add (d,t) !f.bl.defs)
 
-let commit_block f = 
-  !f.pr.blocks <- !f.pr.blocks@[!f.bl]; 
-  !f.bl <- {!f.bl with stmts = []} 
+let init_fn name params ret: funwork = {procs = []; bl = {stmts = []; defs = params} ; pr = {name=name; params=params; blocks = []; updated = ExprSet.empty; rt_return = ret; returning = false; stmt_count = 0}} 
 
-let params_call defs = List.map (fun (d, t) -> Expr_Var d) (DefSet.to_list defs) 
-
-let add_procs f p = !f.procs <- !f.procs @ p 
-
-let commit_proc f = 
-    let n_name = new_name () in
-    commit_block f; 
-    !f.procs <- !f.procs@[!f.pr] ; 
-    !f.pr <- {name = n_name ; params = !f.bl.defs; blocks = [{!f.bl with stmts = []}]}
-
-let rec split_stmt s (f: funwork ref) : unit =  
-  (match s with
-    | Stmt_VarDeclsNoInit(ty, vs, loc) -> add_stmt f s  ; List.iter (fun d -> add_def f d ty ) vs 
-    | Stmt_VarDecl(ty, v, e, loc) -> add_stmt f s  ; add_def f v ty 
-    | Stmt_ConstDecl(ty, v, e, loc) -> add_stmt f s ; add_def f v ty  
-    | Stmt_FunReturn(e, loc) ->add_stmt f s ; commit_block f 
-    | Stmt_ProcReturn(loc) ->add_stmt f s ; commit_block f 
-    | Stmt_If(c, thensl, elsesl, ff, loc) ->
-        commit_block f ;
-        (* unconditionally split the ifcond subblocks into separate blocks *)
-        let rec iter = function
-        | S_Elsif_Cond(c,b)::xs ->
-            S_Elsif_Cond(c, split_stmts b f)::(iter xs)
-        | [] -> [] in
-        let thennew = (split_stmts thensl f) in
-        let elsenew = iter elsesl in
-        let ffnew = split_stmts ff f in
-        add_stmt f (Stmt_If(c, thennew, elsenew, ffnew, loc))
-    | _ -> ()) 
-
-and split_stmts (ss: stmt list) (f: funwork ref) : stmt list = 
-  match ss with
-    | [] -> ss
-    | xs -> 
-      let nf = ref (init_fn (new_name ()) !f.bl.defs) in
-      let cf = !nf.pr in
-      List.iter (fun s -> split_stmt s nf) xs; commit_proc nf; (add_procs f !nf.procs) ;
-      [Stmt_TCall(cf.name, [], (params_call cf.params), Unknown)]
+let split_stmts (ss: stmt list) (f: funwork ref) : stmt list = 
+  let param_tuple (ff: funwork) : expr list = (get_proc_return_sig ff.pr |> List.map (fun (f,t) -> Expr_Var (f)))  in
+  let param_l_tuple (ff: funwork) : lexpr list = (get_proc_return_sig ff.pr |> List.map (fun (f,t) -> LExpr_Var (f)))  in
+  let add_updated name st = 
+      let is_param x = List.mem x (param_l_tuple !f)  in
+      if (is_param name) then (!st.pr.updated <- ExprSet.add name !st.pr.updated) in
+  (*let empty_block _ = {stmts=[]; defs = DefSet.empty} in
+  let continue_block b = {b with stmts=[]} in *)
+  let new_name _ = Ident ( "proc_" ^ (string_of_int (new_index ())))  in
+  let add_stmt f (s) = !f.bl.stmts <- (!f.bl.stmts @ [s])  in
+  let add_def f d t mble = !f.bl.defs <- (DefSet.add (d,t,mble) !f.bl.defs) in
+  let commit_block f = 
+    !f.pr.blocks <- !f.pr.blocks@[!f.bl]; 
+    !f.bl <- {!f.bl with stmts = []}  in
+  let params_call defs = (DefSet.to_list defs |> List.map (function 
+      | (d,t,_) -> Expr_Var (d)
+    ))  in
+  let add_procs f p = !f.procs <- !f.procs @ p  in
+  let rec split_stmts (ss: stmt list) (f: funwork ref) : stmt list = 
+    let commit_proc f = 
+        let n_name = new_name () in
+        if (Bool.not( !f.pr.returning)) then add_stmt f (split_stmt (Stmt_ProcReturn Unknown) f) ;
+        commit_block f; 
+        !f.procs <- !f.procs@[!f.pr] ; 
+        !f.pr <- {name = n_name ; params = !f.bl.defs; blocks = [{!f.bl with stmts = []}]; updated = ExprSet.empty; rt_return = !f.pr.rt_return; returning = false; stmt_count = 0} in
+    let chunk_list chunksize (ss : 'a list) : 'a list list = if ((List.length ss) < chunksize) then [ss] else 
+      List.fold_left (fun a b -> match (a,b) with
+        | (hd::tl, r) when (List.length hd) < chunksize -> (hd @ r)::tl
+        | (_,r) -> [r]
+      ) [[]] (ss |> List.map (fun f -> [f]))
+      in
+    (chunk_list 5 ss) |> List.map (fun ss ->
+      (match ss with
+      | [] -> ss
+      | xs -> 
+        let nf = ref (init_fn (new_name ()) !f.bl.defs None) in
+        let cf = !nf.pr in
+        List.iter (fun s -> add_stmt nf (split_stmt s nf)) xs; commit_proc nf; 
+        if  (cf.stmt_count < 1) then ss else (
+          (add_procs f !nf.procs);
+          !f.pr.returning <- cf.returning; (*if called function returns value, we return value *)
+          (*let ls = if (cf.returning) then [(LExpr_Var return_temp)] else [] in
+          let ls = ls @ ((DefSet.to_list cf.params |> List.map fst |> List.map (fun c -> LExpr_Var (c)))) in  *)
+          let rs = get_proc_return_sig cf  in
+          let ls = rs |> List.map (fun (i,_) -> LExpr_Var i) in
+          let expls = rs |> List.map (fun (i,_) -> Expr_Var i) in
+          (match ls with 
+            | [] -> [(Stmt_TCall(cf.name, [], (params_call cf.params), Unknown))]
+            | ls -> (
+          Stmt_Assign (LExpr_Tuple(ls), Expr_TApply(cf.name, [], (params_call cf.params)), Unknown)  ::
+          (match (get_proc_return_value cf) with 
+            | [] ->  (* procedure func just update params *)    [split_stmt (Stmt_ProcReturn Unknown) f] 
+            | _ ->  (* *real* func, return a value *)           [split_stmt (Stmt_FunReturn (List.hd expls, Unknown)) f] )
+        )))))
+    |> List.concat
+  and split_stmt (s: stmt) (f: funwork ref) : stmt =  
+    !f.pr.stmt_count  <- !f.pr.stmt_count  + 1;
+    (match s with
+      | Stmt_VarDeclsNoInit(ty, vs, loc) ->  List.iter (fun d -> add_def f d ty true) vs ; s
+      | Stmt_VarDecl(ty, v, e, loc) -> add_def f v ty true ;s 
+      | Stmt_ConstDecl(ty, v, e, loc) -> add_def f v ty false ; s
+      | Stmt_FunReturn(e, loc) -> 
+        let new_ret : expr = Expr_Tuple (e::(param_tuple !f))   in
+        !f.pr.returning <- true ;
+        let s = (Stmt_FunReturn (new_ret,loc)) in
+         s
+      | Stmt_ProcReturn(loc) -> 
+        !f.pr.returning <- false;
+        let new_ret : expr = Expr_Tuple  (param_tuple !f) in
+        let s = (Stmt_FunReturn (new_ret,loc)) in
+        s
+      | Stmt_Assign(l, r, loc) -> add_stmt f s ;  add_updated (l) f ; s
+      | Stmt_If(c, thensl, elsesl, ff, loc) ->
+          (* unconditionally split the ifcond subblocks into separate blocks *)
+          let rec iter = function
+          | S_Elsif_Cond(c,b)::xs ->
+              S_Elsif_Cond(c, split_stmts b f)::(iter xs)
+          | [] -> [] in
+          let thennew = (split_stmts thensl f) in
+          let elsenew = iter elsesl in
+          let ffnew = split_stmts ff f in
+          let s = (Stmt_If(c, thennew, elsenew, ffnew, loc)) in
+          s
+      | _ -> s) in
+  split_stmts ss f 
 
 
 let rec write_stmt s st =
@@ -542,38 +638,41 @@ let rinit_st oc st : st =  {st with indent = 0; skip_seq=false;  oc=oc;  definit
 
 let build_args (tys: ((ty * ident)  list)) targs args =
   let targs =  List.map (fun t -> name_of_ident t) targs in 
-  let ta = List.map (fun (t, i) -> (name_of_ident i) ^ ": " ^ (prints_arg_type t)) tys in
+  let args =  List.map (fun t -> name_of_ident t) args in 
+  let ta = List.map (fun (t, i) -> "_" ^ (name_of_ident i) ^ ": " ^ (prints_arg_type t)) tys in
   if List.length targs = 0 && List.length args = 0 then "()"
   else "(" ^ (String.concat "," (targs@ta)) ^ ")" 
 
-
-
-  
+let build_args_mut (targs: ident list) (args: ident list) st =
+  List.iter (fun (i) -> write_line (Printf.sprintf "var %s = _%s\n"  (i)  (i)) st) (targs@args |> List.map name_of_ident)
 
 let print_write_fn (name: AST.ident) (ret_tyo, argtypes, targs, args, _, body) st = 
   update_funcalls {name=(name_of_ident name); 
     targs=(List.map (fun _ -> Some (Type_Constructor (Ident "integer"))) targs); 
     args = List.map (fun (t, _) -> Some t) (argtypes)} (st.output_fun) ;
-  let args = build_args argtypes targs args in
+  let wargs = build_args argtypes targs args in
   let ret = prints_ret_type ret_tyo in
-  Printf.fprintf st.oc "def %s %s : %s = {\n" (name_of_ident name) args ret;
+  Printf.fprintf st.oc "def %s %s %s = {\n" (name_of_ident name) wargs ret;
+  inc_depth st ;
+  build_args_mut targs args st;
+  dec_depth st ;
   write_stmts body st;
   Printf.fprintf st.oc "\n}\n"
 
 
-
-let to_binding (p:proc) : (AST.ident * ('a option) * ((ty * ident) list) * (ident list)  * 'b * (stmt list) ) = 
-  let argtypes : (ty * ident ) list = List.map (fun (b,a) -> (a,b)) (DefSet.to_list p.params) in
-  let args = (List.map snd (DefSet.to_list p.params)) in
-  (p.name, None,  argtypes, [], args, List.concat (List.map (fun f -> f.stmts) p.blocks) ) 
+let proc_to_funsig (p:proc) : (AST.ident * ('a option) * ((ty * ident) list) * (ident list)  * 'b * (stmt list) ) = 
+  let argtypes : (ty * ident ) list = List.map (fun (b,a,_) -> (a,b)) (DefSet.to_list p.params) in
+  let args = (List.map (fun (a,b,c) -> a) (DefSet.to_list p.params)) in
+  let rt = get_proc_return_type p in
+  (p.name, rt,  argtypes, [], args, List.concat (List.map (fun f -> f.stmts) p.blocks) ) 
 
 let write_fn (name: AST.ident) (ret_tyo, argtypes, targs, args, _, body) st = 
-  let bs = DefSet.of_list (List.map (fun (a,t) -> (t,a)) argtypes) in
-  let b = ref @@ init_fn name bs in
+  let bs = DefSet.of_list (List.map (fun (a,t) -> (t,a, false)) argtypes) in
+  let b = ref @@ init_fn name bs (Some ret_tyo) in
   let nb = split_stmts body b in
   print_write_fn name (ret_tyo, argtypes, targs, args, (), nb) st;
   List.iter (fun p -> 
-    let nm, rt, art, targs, args , body = to_binding p in
+    let nm, rt, art, targs, args , body = proc_to_funsig p in
     print_write_fn (nm) (rt,art,targs,args,(),body) st
   ) !b.procs
 
@@ -640,7 +739,7 @@ let printfundiff st =
    *)
 
 
-let run (dfn: AST.ident) dfnsig tests (fns : (ty option * (ty * ident) list * ident list * 'a list * 'b * stmt list) Bindings.t) dir : unit =
+let run (dfn : ident) (dfnsig : ty option * 'a * ident list * ident list * 'b * stmt list) (tests : (ty option * 'a * ident list * ident list * 'b * stmt list) Bindings.t) (fns : (ty option * 'a * ident list * ident list * 'b * stmt list) Bindings.t) (dir : typeid) =
   let st = init_st  in
   let files = Bindings.fold (fun fn fnsig acc -> (write_instr_file fn fnsig dir st)::acc) fns [] in
   let files = (write_test_file tests dir st)::files in
